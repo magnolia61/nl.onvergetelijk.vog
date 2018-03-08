db0d63e533688a5776be65c49675ab7545a4a86d
magnolia61 <richard.van.oosterhout@gmail.com>
2018-03-08 10:23:01 +0100

    verbeteringen mbt participant naar cv

--- vog.php
+++ vog.php
@@ -12,19 +12,6 @@ require_once 'vog.civix.php';
  * This is needed only if there is a computed (View Only) custom field in this set.
  */
 
-function vog_civicrm_post( $op, $objectName, $objectId, &$objectRef ) {
-  if ($objectName == "Participant") {
-    #watchdog('callhooks',"callhooks_civicrm_post_${op}_${objectName}(\$objectId, &\$objectRef)<pre>\n\$objectId=$objectId\n\$objectRef=" . var_export($objectRef,1) . '</pre>');
-    #watchdog('php', '<pre>--- START VOG CIVICRM POST ---</pre>', NULL, WATCHDOG_DEBUG);
-    #if (function_exists("callhooks_civicrm_post_${op}_$objectName"))
-    # { $tz = date_default_timezone_get();
-    #  date_default_timezone_set('UTC');
-    # call_user_func("callhooks_civicrm_post_${op}_$objectName", $objectId, $objectRef);
-    # date_default_timezone_set($tz);
-    #}
-  }
-}
-
 function vog_civicrm_custom( $op, $groupID, $entityID, &$params ) {
 
   #watchdog('php', '<pre>--- START FUNCTION VOG ---</pre>', NULL, WATCHDOG_DEBUG);
@@ -33,25 +20,23 @@ function vog_civicrm_custom( $op, $groupID, $entityID, &$params ) {
     return;									        //	if not, get out of here
   }
   if ($groupID == 140 && $op == 'edit') {						//	group ID of the Curriculum custom field set
-    watchdog('php', '<pre>op:'. print_r($op, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
-    watchdog('php', '<pre>id:'. print_r($groupID, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>op:'. print_r($op, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>id:'. print_r($groupID, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
 
     watchdog('php', '<pre>--- START EXTENSION VOG ---</pre>', NULL, WATCHDOG_DEBUG);
 	$tableName1	= "civicrm_value_part_leid_vog_140";			//	table name for the custom group (each set of custom fields has a corresponding table in the database)
 	$tableName2	= "civicrm_value_intake_181";				//	table name for the custom group (each set of custom fields has a corresponding table in the database)
 	$tableName3	= "civicrm_value_part_leid_190";			//	table name for the custom group (each set of custom fields has a corresponding table in the database)
-	
 
     $sql1 = "SELECT vp.datum_laatste_vog_603 AS voglaatste, vp.kenmerk_vog_602 AS vogkenmerk, vp.scan_foto_van_je_vog_604 AS vogscan, pt.contact_id AS contactid, pt.event_id AS eventid FROM $tableName1 AS vp INNER JOIN `civicrm_participant` AS pt ON vp.entity_id = pt.id WHERE vp.entity_id = '$entityID'";
-    watchdog('php', '<pre>sql1:'. print_r($sql1, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>sql1:'. print_r($sql1, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     $dao1 = CRM_Core_DAO::executeQuery( $sql1 );
     #watchdog('php', '<pre>dao1:'. print_r($dao1, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
 
     $sql2 = "SELECT lp.functie_568 AS kampfunctie, lp.voor_het_eerst_649 AS eerstekeer, vp.entity_id FROM $tableName1 AS vp INNER JOIN $tableName3 AS lp ON vp.entity_id = lp.entity_id WHERE vp.entity_id = '$entityID'";
-    watchdog('php', '<pre>sql2:'. print_r($sql2, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>sql2:'. print_r($sql2, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     $dao2 = CRM_Core_DAO::executeQuery( $sql2 );
     #watchdog('php', '<pre>dao2:'. print_r($dao2, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
-    
     while ($dao1->fetch()) {
       $contactid       = $dao1->contactid;
       $eventid         = $dao1->eventid;
@@ -65,12 +50,12 @@ function vog_civicrm_custom( $op, $groupID, $entityID, &$params ) {
     }
 
     $sql3 = "SELECT start_date AS eventstart FROM civicrm_event WHERE id = '$eventid'";
-    watchdog('php', '<pre>sql3:'. print_r($sql3, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>sql3:'. print_r($sql3, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     $dao3 = CRM_Core_DAO::executeQuery( $sql3 );
     #watchdog('php', '<pre>dao3:'. print_r($dao3, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
 
     $sql4 = "SELECT datum_meest_recente_vog_56 AS vogrecent, kenmerk_vog_68 AS kenmerkrecent FROM $tableName2 WHERE entity_id = '$contactid'";
-    watchdog('php', '<pre>sql4:'. print_r($sql4, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>sql4:'. print_r($sql4, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     $dao4 = CRM_Core_DAO::executeQuery( $sql4 );
     #watchdog('php', '<pre>dao1:'. print_r($dao4, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
 
@@ -96,13 +81,13 @@ function vog_civicrm_custom( $op, $groupID, $entityID, &$params ) {
     if ($vogrecent   == NULL)		{ $vognodig = 'eerstex'; }
     if (stristr($eerstekeer, 'eerstekeer') !== FALSE) { $vognodig = 'eerstex'; }
 
-    watchdog('php', '<pre>date1:'. print_r($date1, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
-    watchdog('php', '<pre>date2:'. print_r($date2, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
-    watchdog('php', '<pre>diff:'. print_r($diff, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>date1:'. print_r($date1, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>date2:'. print_r($date2, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>diff:'. print_r($diff, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     watchdog('php', '<pre>eerstekeer:'. print_r($eerstekeer, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
-    watchdog('php', '<pre>diffyears:'. print_r($diffyears, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
-    watchdog('php', '<pre>diffmonths:'. print_r($diffmonths, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
-    watchdog('php', '<pre>diffmonthstotal:'. print_r($diffmonthstotal, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>diffyears:'. print_r($diffyears, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>diffmonths:'. print_r($diffmonths, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
+    #watchdog('php', '<pre>diffmonthstotal:'. print_r($diffmonthstotal, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     watchdog('php', '<pre>vognodig:'. print_r($vognodig, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     watchdog('php', '<pre>functie:'. print_r($functiekamp, TRUE) .'</pre>', NULL, WATCHDOG_DEBUG);
     $sql5 	= "UPDATE $tableName1 SET vog_nodig_586 = '$vognodig' WHERE entity_id = '$entityID'";
